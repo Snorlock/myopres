bc1bca5033cbcaa2ea4c6b0fd9891eb4128d532a
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2015-03-31 23:37:19 +0200

    updated the press, also added the asm.jar file to show my android screen

--- plugin/myo/myo.js
+++ plugin/myo/myo.js
@@ -1,5 +1,4 @@
 (function () {
-	console.log("myo");
 	var myFirebaseRef = new Firebase("https://myopres.firebaseio.com/");
 	myFirebaseRef.child("pose").on("value", function(snapshot){
 		if(snapshot.val() === "WAVE_IN") {
@@ -7,6 +6,12 @@
 		}
 		else if (snapshot.val() === "WAVE_OUT") {
 			Reveal.left();
+		}
+		else if (snapshot.val() === "FIST") {
+			Reveal.down();
+		}
+		else if (snapshot.val() === "FINGERS_SPREAD") {
+			Reveal.up();
 		}
 	})
 })()
